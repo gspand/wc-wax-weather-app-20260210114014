1266460342348d230540a9960d6e0cfb66c9a5e4
GspandJn <gspandjn@users.noreply.github.com>
2026-02-10 15:40:23 +0100

    Device location hint + clear prefilled search

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = "alpenwetter-v8";
-const RUNTIME_CACHE = "alpenwetter-runtime-v8";
+const CACHE_NAME = "alpenwetter-v9";
+const RUNTIME_CACHE = "alpenwetter-runtime-v9";
 
 const APP_SHELL = [
     "./",
