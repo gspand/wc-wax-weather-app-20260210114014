b274a4009572fc4ed3e7b5d3cfffe7e7b5b4225a
GspandJn <gspandjn@users.noreply.github.com>
2026-02-10 16:05:33 +0100

    Refresh only when new datasets

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = "alpenwetter-v10";
-const RUNTIME_CACHE = "alpenwetter-runtime-v10";
+const CACHE_NAME = "alpenwetter-v11";
+const RUNTIME_CACHE = "alpenwetter-runtime-v11";
 
 const APP_SHELL = [
     "./",
